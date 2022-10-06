a3b78857abb4762d3bde0d21316ce39637f5b5c0
elementtime6969 <willizbruce@gmail.com>
2022-10-06 12:46:59 +0100

    hello

--- name.py
+++ name.py
@@ -1,10 +1,15 @@
 print('welcome to my world')
 
 username = input('please enter yourname:')
-print(username)
+print('NICE NAME',username)
 
-print('nice,now lets move on')
+print('nice,now lets move on',)
 
-age = int(input('enter your age'))
+age = int(input('enter your age:'))
 print(age)
-print(type(age))
+if age >= 18:
+    print('GOOD YOU ARE ABOVE THE RANKED AGE NEEDED')
+elif age == 17:
+    print('HMM MAYBE YOU CAN PROGRESS')
+else:
+    print('GET OUT KID')
